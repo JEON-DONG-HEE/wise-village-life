c7956143a719f44bff9401f724a69cbed5cd1ced
전동희 <bethetopone@gmail.com>
2026-05-28 16:12:57 +0900

    슬기로운 마을 생활 프로젝트 초기 커밋

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>슬기로운 마을 생활</h1>
+      <p>마을 주민 관리 화면을 만들어봅니다.</p>
+    </div>
+  );
+}
+
+export default App;
